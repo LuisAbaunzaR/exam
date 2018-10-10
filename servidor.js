35e06ca6b71a10809274a8eb3ba839eae70577db
Luis Abaunza <luis@soma.mx>
2018-10-09 20:27:59 -0500

    corrigiendo port por PORT

--- servidor.js
+++ servidor.js
@@ -100,7 +100,7 @@ app.delete("/api/student/:id/", (request, response) => {
 
 
 //usar en local host
-var PORT = process.env.port || 8801;
+var PORT = process.env.PORT || 8801;
  app.listen(PORT,()=>{
     console.log(`Servidor Corriendo en el puerto ${PORT}`)
  })
